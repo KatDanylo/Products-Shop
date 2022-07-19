5be9ec14ea81575545e54f94993adc5626035878
Kateryna Danylo <aleksandrova414@gmail.com>
2022-07-19 10:56:42 +0300

    add delete btn

--- index.js
+++ index.js
@@ -35,21 +35,33 @@
     const modal_window = document.querySelector('.modal');
     const cart_link = document.querySelector('#cart_link');
     const store_link = document.querySelector('#store_link');
-    const order_count = document.querySelector('.order_count');
+
  
     productsContainer.addEventListener('click', function(event){
         if (event.target.closest('.btn_byu')){
-            const orderData = document.querySelector('.order_data');
+            const order_data = document.querySelector('.order_data');
             for (const cartData of products){
-            orderData.innerHTML += `
+            order_data.innerHTML += `
             <div class="order_item">
                 <img src="${cartData.url}" alt="${cartData.name}  ">
                 <p class="category"> ${cartData.category}:  ${cartData.name}</p>
-                <p class="price"> |  ${cartData.price}$</p>
+                <p class="price"> ${cartData.price}$</p>
                 <input  class="order_count" type="number" min="1" step="1" value="1">
+                <img class="btn_delete" src="img/delete.png" alt="Delete">
             </div> `;
              }
             modal_window.classList.toggle("visible");
+            const order_product = document.querySelector('.order_item');
+            console.log(order_product);
+            order_product.addEventListener('click', function(event){
+                if(event.target.closest('.btn_delete')){
+                    order_data -=  order_product.target;
+                    renderProducts(cartData)
+                    console.log(order_data, order_product.target)
+                }
+                })
+       
+
         }
     });
     storeContainer.addEventListener('click', function(event){
